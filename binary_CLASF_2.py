29d37ae948e9c68d63c93ffe8edb9d56e70de5eb
JohnnyLiao <highhand31@gmail.com>
2018-11-30 14:25:52 +0800

    add print message
    (cherry picked from commit c60eb99)

--- binary_CLASF_2.py
+++ binary_CLASF_2.py
@@ -426,6 +426,5 @@ if __name__ == "__main__":
     CLASF.train_addr(x_train_addr, x_train_label, x_test_addr, x_test_label, GPU_ratio=GPU_ratio, epochs=epochs,
                 batch_size=batch_size, fine_tune=False)
 
-    print("This is test5 for github trail!")
 
-
+    print("github test")
